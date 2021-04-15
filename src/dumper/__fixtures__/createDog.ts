d38eaf259a48769106e6134c734dad024c617f4e
Streeterxs <afonso_mg3@hotmail.com>
2021-04-14 22:21:30 -0300

    feat(create-document-fixtures): functions to make test easier

---
+++ src/dumper/__fixtures__/createDog.ts
@@ -0,0 +1,18 @@
+import Dog, { IDog } from './DogModel';
+
+export const createDog = async (args?: IDog) => {
+    const { name = 'mock-dog-name', toys = [], ...properties } = args;
+
+    await new Dog({ name, toys, ...properties }).save();
+};
+
+export const getOrCreateDog = async () => {
+    const dogFinded = await Dog.findOne();
+
+    if (!dogFinded) {
+        const newDog = await createDog();
+        return newDog;
+    }
+
+    return dogFinded;
+};
