27c76053d88e0b13a0f5b2052e305ebdc3f46055
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2022-01-08 20:35:37 -0300

    feat(release-patch-pull-request): added pull request feature to release-patch script (#78)

--- scripts/release/release-patch.js
+++ scripts/release/release-patch.js
@@ -6,6 +6,7 @@ const util = require('util');
 const fs = require('fs');
 
 const execPromisified = util.promisify(childProcess.exec);
+const spawnPromisified = util.promisify(childProcess.spawn);
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
@@ -24,21 +25,32 @@ const writeFile = util.promisify(fs.writeFile);
       tag: `${latest}..main`,
     });
 
+    const branchName = `release/version-${newVersion}`;
+    await git.pull('origin', 'main');
+    await git.checkoutBranch(branchName, 'main');
+
     const actualChangelog = await readFile('./CHANGELOG.md', 'utf8');
     const newChangelog = generatedChangelog + actualChangelog;
     await writeFile('./CHANGELOG.md', newChangelog);
 
-    await git.checkoutBranch(`release-${newVersion}`);
     await execPromisified(
       `yarn version --new-version ${newVersion} --no-git-tag-version`
     );
 
     await git.add(['package.json', 'CHANGELOG.md']);
     await git.commit(`release(version): ${newVersion}`);
+    await git.push('origin', branchName);
 
-    await execPromisified(
-      `gh pr create --title "release(version): ${newVersion}"`
-    );
+    const { spawn } = childProcess;
+    const gh = spawn('gh', [
+      'pr',
+      'create',
+      '--title',
+      `"release(version): ${newVersion}"`,
+      '--body',
+      generatedChangelog,
+    ]);
+    gh.stdout.on('data', () => gh.stdin.write('\n'));
   } catch (err) {
     console.log({ err });
   }
