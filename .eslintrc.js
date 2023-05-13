5642129eedad63ce559cec0a19f9e3b0639422ad
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-05-13 17:07:39 -0300

    feat(eslint): added no-unused-expressions rule (#111)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "jest",
-        "no-only-tests"
-    ],
-    "rules": {
-        "jest/no-test-prefixes": "error",
-        "no-only-tests/no-only-tests": "error"
-    }
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'jest', 'no-only-tests'],
+  rules: {
+    'jest/no-test-prefixes': 'error',
+    'no-only-tests/no-only-tests': 'error',
+    'no-unused-expressions': 'error',
+  },
 };
