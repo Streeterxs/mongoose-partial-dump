ec7273ec899aa54adb539b2fc3a8a1451c0fb61a
Afonso Araújo Neto <afonso_mg3@hotmail.com>
2023-05-14 11:53:43 -0300

    feat(chunk-restore): added insertMany docs in restore by chunk (#112)
    * feat(chunk-restore): added insertMany docs in restore by chunk
    
    * feat(chunk-restore): updating yarn lock file

--- src/restorer/restorer.ts
+++ src/restorer/restorer.ts
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
+import { chunk } from 'lodash';
 
+const CHUNK_SIZE = 50000;
 type RestoreData = {
-   [key: string]: any[];
+  [key: string]: any[];
 };
 export const restore = async (dataString: string) => {
-   const data: RestoreData = JSON.parse(dataString);
+  const data: RestoreData = JSON.parse(dataString);
 
-   const collectionNameArray = Object.keys(data);
+  const collectionNameArray = Object.keys(data);
 
-   for (const collectionName of collectionNameArray) {
-      const model = mongoose.model(collectionName);
+  for (const collectionName of collectionNameArray) {
+    const model = mongoose.model(collectionName);
 
-      for (const document of data[collectionName]) {
-         await new model({ ...document }).save();
-      }
-   }
+    const dataChunks = chunk(data[collectionName], CHUNK_SIZE);
+    for (const dataChunk of dataChunks) {
+      await model.insertMany(dataChunk);
+    }
+  }
 };
