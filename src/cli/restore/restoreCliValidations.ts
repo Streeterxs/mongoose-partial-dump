3f845b22841f35e568a1068203e13a393912a567
Streeterxs <afonso_mg3@hotmail.com>
2023-05-15 17:22:36 -0300

    feat(restore-values): added defaultValues and defaultSingValues to restore

--- src/cli/restore/restoreCliValidations.ts
+++ src/cli/restore/restoreCliValidations.ts
@@ -2,85 +2,87 @@ import { RestoreConfig } from '../../configs/restoreConfig/restoreConfigValidati
 import { CliRestore } from '../cliTypes';
 
 export type CliValidation<T> = {
-   errors: (string | null)[];
-   argv: Nullable<T> | null;
-   warnings: (string | null)[];
+  errors: (string | null)[];
+  argv: Nullable<T> | null;
+  warnings: (string | null)[];
 };
 
 export const restoreCliValidations = (
-   argv: CliRestore
+  argv: CliRestore
 ): CliValidation<CliRestore> => {
-   const inputDirValidatedObj = inputDirCliValidate(argv.inputDir);
+  const inputDirValidatedObj = inputDirCliValidate(argv.inputDir);
 
-   const errors = [inputDirValidatedObj.error];
+  const errors = [inputDirValidatedObj.error];
 
-   const warnings = [inputDirValidatedObj.warning];
+  const warnings = [inputDirValidatedObj.warning];
 
-   const nonNullErrors = errors.filter((error) => error !== null);
-   const nonNullWarnings = warnings.filter((error) => error !== null);
-   return {
-      errors: nonNullErrors,
-      warnings: nonNullWarnings,
-      argv: {
-         inputDir: inputDirValidatedObj.inputDir,
-      },
-   };
+  const nonNullErrors = errors.filter((error) => error !== null);
+  const nonNullWarnings = warnings.filter((error) => error !== null);
+  return {
+    errors: nonNullErrors,
+    warnings: nonNullWarnings,
+    argv: {
+      inputDir: inputDirValidatedObj.inputDir,
+    },
+  };
 };
 
 const inputDirCliValidate = (inputDir: string | undefined) => {
-   if (!inputDir) {
-      return {
-         error: null,
-         warning:
-            'warning: <inputDir> argv was not defined. You must input a <inputDir> on CONFIG for the dump work.',
-         db: null,
-      };
-   }
-
-   if (!(typeof inputDir === 'string')) {
-      return {
-         error: null,
-         warning:
-            'warning: <inputDir> argv is defined, but not a function. This will cause a error if no <inputDir> input was given on CONFIG.',
-         inputDir: null,
-      };
-   }
-
-   return {
+  if (!inputDir) {
+    return {
       error: null,
-      warning: null,
-      inputDir,
-   };
+      warning:
+        'warning: <inputDir> argv was not defined. You must input a <inputDir> on CONFIG for the dump work.',
+      db: null,
+    };
+  }
+
+  if (!(typeof inputDir === 'string')) {
+    return {
+      error: null,
+      warning:
+        'warning: <inputDir> argv is defined, but not a function. This will cause a error if no <inputDir> input was given on CONFIG.',
+      inputDir: null,
+    };
+  }
+
+  return {
+    error: null,
+    warning: null,
+    inputDir,
+  };
 };
 
 export const validateRestoreCliConfig = (
-   argv: void | Nullable<CliRestore>,
-   config: void | Nullable<RestoreConfig> | undefined
+  argv: void | Nullable<CliRestore>,
+  config: void | Nullable<RestoreConfig> | undefined
 ) => {
-   if (!argv) {
-      console.log('error: Invalid given CLI arguments');
+  if (!argv) {
+    console.log('error: Invalid given CLI arguments');
+    return;
+  }
+
+  if (!config) {
+    console.log('error: Invalid given CONFIG arguments');
+    return;
+  }
+
+  if (!argv.inputDir) {
+    if (!config.inputDir) {
+      console.log(
+        'error: At least a CONFIG <inputDir> or a CLI <inputDir> must me given'
+      );
       return;
-   }
+    }
+  }
 
-   if (!config) {
-      console.log('error: Invalid given CONFIG arguments');
-      return;
-   }
+  const inputDir = argv.inputDir ?? config.inputDir;
 
-   if (!argv.inputDir) {
-      if (!config.inputDir) {
-         console.log(
-            'error: At least a CONFIG <inputDir> or a CLI <inputDir> must me given'
-         );
-         return;
-      }
-   }
-
-   const inputDir = argv.inputDir ?? config.inputDir;
-
-   return {
-      models: config.models,
-      db: config.db,
-      inputDir,
-   };
+  return {
+    models: config.models,
+    db: config.db,
+    defaultValues: config.defaultValues,
+    defaultSingValues: config.defaultSingValues,
+    inputDir,
+  };
 };
--- src/configs/restoreConfig/restoreConfigValidations.ts
+++ src/configs/restoreConfig/restoreConfigValidations.ts
@@ -1,81 +1,85 @@
 import { Model } from 'mongoose';
 import { DatabaseConfig } from '../../database/database';
 import {
-   ConfigValidation,
-   dbConfigValidate,
-   modelsConfigValidate,
+  ConfigValidation,
+  dbConfigValidate,
+  modelsConfigValidate,
 } from '../basicValidations';
 
 export type RestoreConfig = {
-   db: DatabaseConfig;
-   models: Model<any>[];
-   inputDir?: string;
+  db: DatabaseConfig;
+  models: Model<any>[];
+  inputDir?: string;
+  defaultValues?: any;
+  defaultSingValues?: any;
 };
 
 export const restoreConfigValidations = (
-   config?: Partial<RestoreConfig>
+  config?: Partial<RestoreConfig>
 ): ConfigValidation<RestoreConfig> => {
-   if (!config) {
-      return {
-         errors: [
-            'error: Configuration file is missing. Example configuration file name: |partial-dump.config.js|',
-         ],
-         warnings: [null],
-         config: null,
-      };
-   }
+  if (!config) {
+    return {
+      errors: [
+        'error: Configuration file is missing. Example configuration file name: |partial-dump.config.js|',
+      ],
+      warnings: [null],
+      config: null,
+    };
+  }
 
-   const dbValidatedObj = dbConfigValidate(config.db);
-   const modelsValidatedObj = modelsConfigValidate(config.models);
-   const inputDirValidatedObj = inputDirConfigValidate(config.inputDir);
+  const dbValidatedObj = dbConfigValidate(config.db);
+  const modelsValidatedObj = modelsConfigValidate(config.models);
+  const inputDirValidatedObj = inputDirConfigValidate(config.inputDir);
 
-   const errors = [
-      dbValidatedObj.error,
-      modelsValidatedObj.error,
-      inputDirValidatedObj.error,
-   ];
+  const errors = [
+    dbValidatedObj.error,
+    modelsValidatedObj.error,
+    inputDirValidatedObj.error,
+  ];
 
-   const warnings = [
-      dbValidatedObj.warning,
-      modelsValidatedObj.warning,
-      inputDirValidatedObj.warning,
-   ];
+  const warnings = [
+    dbValidatedObj.warning,
+    modelsValidatedObj.warning,
+    inputDirValidatedObj.warning,
+  ];
 
-   const nonNullErrors = errors.filter((error) => error !== null);
-   const nonNullWarnings = warnings.filter((error) => error !== null);
-   return {
-      errors: nonNullErrors,
-      warnings: nonNullWarnings,
-      config: {
-         db: dbValidatedObj.db,
-         models: modelsValidatedObj.models,
-         inputDir: inputDirValidatedObj.inputDir,
-      },
-   };
+  const nonNullErrors = errors.filter((error) => error !== null);
+  const nonNullWarnings = warnings.filter((error) => error !== null);
+  return {
+    errors: nonNullErrors,
+    warnings: nonNullWarnings,
+    config: {
+      db: dbValidatedObj.db,
+      models: modelsValidatedObj.models,
+      inputDir: inputDirValidatedObj.inputDir,
+      defaultValues: config.defaultValues,
+      defaultSingValues: config.defaultSingValues,
+    },
+  };
 };
 
 const inputDirConfigValidate = (inputDir: string | undefined) => {
-   if (!inputDir) {
-      return {
-         error: null,
-         warning:
-            'warning: <inputDir> was not defined on [CONFIG]. You must input a <inputDir> on CLI for the dump work.',
-         db: null,
-      };
-   }
-
-   if (!(typeof inputDir === 'string')) {
-      return {
-         error: null,
-         warning:
-            'warning: <inputDir> config is defined, but not a function on [CONFIG]. This will cause a error if no <inputDir> input was given on CLI.',
-         inputDir: null,
-      };
-   }
-
-   return {
+  if (!inputDir) {
+    return {
       error: null,
-      warning: null,
-      inputDir,
-   };
+      warning:
+        'warning: <inputDir> was not defined on [CONFIG]. You must input a <inputDir> on CLI for the dump work.',
+      db: null,
+    };
+  }
+
+  if (!(typeof inputDir === 'string')) {
+    return {
+      error: null,
+      warning:
+        'warning: <inputDir> config is defined, but not a function on [CONFIG]. This will cause a error if no <inputDir> input was given on CLI.',
+      inputDir: null,
+    };
+  }
+
+  return {
+    error: null,
+    warning: null,
+    inputDir,
+  };
 };
